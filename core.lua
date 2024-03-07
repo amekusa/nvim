7276a99d550bf620e6cc8f71131fb8d22cc4836d
amekusa <hello@amekusa.com>
2024-03-07 12:11:15 +0900

    update core

--- core.lua
+++ core.lua
@@ -1,44 +1,34 @@
--- hide those ugly '~' (tilde) symbols
-vim.wo.fillchars="eob: "
-
--- set space as the <leader> key
+---- KEY BINDINGS ----
+-- space: <leader> key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+-- (normal mode) esc: clear search highlight
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- nerd font support
-vim.g.have_nerd_font = false
-
--- line numbers
-vim.opt.number = true
-vim.opt.relativenumber = false
-
--- mouse support ('a' for all modes)
-vim.opt.mouse = 'a'
-
--- mode indicator
-vim.opt.showmode = true
-
+---- GENERAL ----
 -- clipboard support
 vim.opt.clipboard = 'unnamedplus'
-
--- break indent
-vim.opt.breakindent = true
-
--- case-insensitive search unless \C or capital in query
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- sign column
-vim.opt.signcolumn = 'yes'
-
+-- mouse support ('a' for all modes)
+vim.opt.mouse = 'a'
 -- decrease update time
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 
+---- UI ----
+-- hide those ugly '~' (tilde) symbols
+vim.wo.fillchars='eob: '
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = false
+-- mode indicator
+vim.opt.showmode = true
+-- sign column
+vim.opt.signcolumn = 'yes'
 -- how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+---- EDITOR ----
 -- show whitespace chars
 vim.opt.list = true
 vim.opt.listchars = {
@@ -46,12 +36,18 @@ vim.opt.listchars = {
 	trail = '·',
 	nbsp  = '␣'
 }
-
+-- break indent
+vim.opt.breakindent = true
 -- preview substitutions as you type
 vim.opt.inccommand = 'split'
-
 -- highlight the current line
 vim.opt.cursorline = true
-
 -- minimal number of screen lines to keep above and below the cursor
 vim.opt.scrolloff = 10
+
+---- SEARCH ----
+-- case-insensitive search unless \C or capital in query
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- set highlight on search
+vim.opt.hlsearch = true
