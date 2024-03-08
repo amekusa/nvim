36508c153bae0b70a405066ce2420a9937f4a912
amekusa <hello@amekusa.com>
2024-03-08 10:22:47 +0900

    maps: add some

--- keymaps.lua
+++ keymaps.lua
@@ -9,11 +9,16 @@ map.set({'n','v'}, ';', ':')
 -- clear search highlight
 map.set('n', '<Esc>', '<Cmd>nohlsearch<CR>')
 -- move line up/down
-map.set('n', '<C-j>', ':m .+1<CR>==')
 map.set('n', '<C-k>', ':m .-2<CR>==')
+map.set('n', '<C-j>', ':m .+1<CR>==')
 -- move line up/down (visual mode)
-map.set('v', '<C-j>', ":m '>+1<CR>gv=gv")
 map.set('v', '<C-k>', ":m '<-2<CR>gv=gv")
-
+map.set('v', '<C-j>', ":m '>+1<CR>gv=gv")
+-- move to prev/next word
+map.set({'n','v'}, '<M-Left>',  'b')
+map.set({'n','v'}, '<M-Right>', 'w')
+-- move to prev/next word (insert mode)
+map.set('i', '<M-Left>',  '<C-o>b')
+map.set('i', '<M-Right>', '<C-o>w')
 -- nvim-tree
 map.set({'n','v','i'}, '<C-n>', '<Cmd>NvimTreeOpen<CR>')
