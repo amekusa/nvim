d5263fe1434d9492ce1849b71fb8a650eb3aab09
amekusa <hello@amekusa.com>
2024-05-16 07:26:41 +0900

    init: move vim options to `options.lua`

--- init.lua
+++ init.lua
@@ -1,10 +1,13 @@
 ---[ amekusa's NeoVim Configuration ]---
 ---- github.com/amekusa/nvim
 
----- GLOBAL ----
+-- global
 local my = {
 	ns = (...), -- namespace
-	conf = { -- config of the config
+	conf = {
+		options = {
+			enable = true,
+		},
 		keymaps = {
 			enable = true,
 		},
@@ -26,90 +29,17 @@ local my = {
 	},
 }
 
+-- root path
+my.path = vim.fn.stdpath('config')..'/lua/'..my.ns:gsub('%.', '/') -- replace '.' in namespace with '/'
+
+-- vim options
+if my.conf.options.enable then require(my.ns..'.options') end
+
 -- custom functions
 my.fn = require(my.ns..'.fn')
 
--- root path
-my.path = vim.fn.stdpath('config')..'/lua/'..
-	my.ns:gsub('%.', '/') -- replace . in namespace with /
-
 -- save to global
 vim.g._custom = my
-
-
----- OS RELATED ----
--- clipboard support
-vim.opt.clipboard = 'unnamedplus'
--- mouse support ('a' for all modes)
-vim.opt.mouse = 'a'
--- enable 24-bit color
-vim.opt.termguicolors = true
-
-
----- MINOR TWEAKS ----
--- wait-time for idle state (ms) [default: 4000]
-vim.opt.updatetime = 1000
--- wait-time for key-combos (ms) [default: 1000]
-vim.opt.timeoutlen = 250
--- redraw the screen lazily [default: false]
-vim.opt.lazyredraw = true
--- default regex engine [default: 0]
-vim.opt.regexpengine = 0
---   0: auto
---   1: old engine
---   2: NFA engine
-
--- fix slow matchparen on large files
-vim.g.matchparen_timeout = 20 -- (ms)
-vim.g.matchparen_insert_timeout = 20 -- (ms)
---   See: https://vi.stackexchange.com/questions/5128/matchpairs-makes-vim-slow/5318#5318
-
-
----- UI ----
--- hide those ugly '~' (tilde) symbols
-vim.opt.fillchars = 'eob: '
--- line numbers
-vim.opt.number = true
-vim.opt.relativenumber = false
--- mode indicator
-vim.opt.showmode = true
--- sign column
-vim.opt.signcolumn = 'yes'
--- how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-
----- EDITOR ----
--- show whitespace chars
-vim.opt.list = true
-vim.opt.listchars = {
-	tab   = '» ',
-	trail = '·',
-	nbsp  = '␣'
-}
--- tab width
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 0  -- 0 falls back to tabstop
--- indent wrapped lines [default: false]
-vim.opt.breakindent = true
--- autoindent new line [default: false]
-vim.opt.smartindent = true
--- preview substitutions as you type
-vim.opt.inccommand = 'split'
--- highlight the current line
-vim.opt.cursorline = true
--- minimal number of screen lines to keep above and below the cursor
-vim.opt.scrolloff = 10
-
-
----- SEARCH ----
--- case-insensitive search unless \C or capital in query
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
--- set highlight on search
-vim.opt.hlsearch = true
-
 
 if my.conf.keymaps.enable then require(my.ns..'.keymaps') end
 if my.conf.plugins.enable then require(my.ns..'.plugins') end
---
+++ options.lua
@@ -0,0 +1,75 @@
+---- OPTIONS ----
+
+-- OS RELATED --
+-- clipboard support
+vim.o.clipboard = 'unnamedplus'
+-- mouse support ('a' for all modes)
+vim.o.mouse = 'a'
+-- enable 24-bit color
+vim.o.termguicolors = true
+
+
+-- MINOR TWEAKS --
+-- wait-time for idle state (ms) [default: 4000]
+vim.o.updatetime = 1000
+-- wait-time for key-combos (ms) [default: 1000]
+vim.o.timeoutlen = 250
+-- redraw the screen lazily [default: false]
+vim.o.lazyredraw = true
+-- default regex engine [default: 0]
+vim.o.regexpengine = 0
+--   0: auto
+--   1: old engine
+--   2: NFA engine
+
+-- fix slow matchparen on large files
+vim.g.matchparen_timeout = 20 -- (ms)
+vim.g.matchparen_insert_timeout = 20 -- (ms)
+--   See: https://vi.stackexchange.com/questions/5128/matchpairs-makes-vim-slow/5318#5318
+
+
+-- UI --
+-- hide those ugly '~' (tilde) symbols
+vim.o.fillchars = 'eob: '
+-- line numbers
+vim.o.number = true
+vim.o.relativenumber = false
+-- mode indicator
+vim.o.showmode = true
+-- sign column
+vim.o.signcolumn = 'yes'
+-- how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+
+-- EDITOR --
+-- show whitespace chars
+vim.o.list = true
+vim.opt.listchars = {
+	tab   = '» ',
+	trail = '·',
+	nbsp  = '␣'
+}
+-- tab width
+vim.o.tabstop = 4
+vim.o.shiftwidth = 0  -- 0 falls back to tabstop
+-- indent wrapped lines
+vim.o.breakindent = true
+-- autoindent new line [default: false]
+vim.o.smartindent = true
+-- preview substitutions as you type
+vim.o.inccommand = 'split'
+-- highlight the current line
+vim.o.cursorline = true
+-- minimal number of screen lines to keep above and below the cursor
+vim.o.scrolloff = 10
+
+
+-- SEARCH --
+-- case-insensitive search unless \C or capital in query
+vim.o.ignorecase = true
+vim.o.smartcase = true
+-- set highlight on search
+vim.o.hlsearch = true
+
