c0ec099d621b4fac7c445b7ce8689faa4ec6d53e
amekusa <hello@amekusa.com>
2024-06-10 05:10:28 +0900

    plugins:surround: use `s` key
    I don't use builtin `s` keymap anyway.

--- plugins.lua
+++ plugins.lua
@@ -600,7 +600,19 @@ local plugins = { -- in alphabetical order
 		lazy = false,
 		config = function()
 			require('nvim-surround').setup({
-				-- options
+				keymaps = {
+					-- insert          = '<C-g>s',
+					-- insert_line     = '<C-g>S',
+					normal          = 's',
+					normal_cur      = 'ss',
+					normal_line     = 'S',
+					normal_cur_line = 'SS',
+					visual          = 's',
+					visual_line     = 'S',
+					-- delete          = 'ds',
+					-- change          = 'cs',
+					-- change_line     = 'cS',
+				},
 			})
 		end
 	},
