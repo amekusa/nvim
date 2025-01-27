553470d3c0c32b4df6e5e7b4d856001ae808d6ac
Satoshi Soma <noreply@amekusa.com>
2025-01-27 23:22:14 +0900

    autocmds: optimize

--- autocmds.lua
+++ autocmds.lua
@@ -4,20 +4,20 @@ local my = _custom
 local map = my.fn.map
 local conf = my.conf.autocmds
 local autocmd = vim.api.nvim_create_autocmd
-
-local function regex_ext(ext) -- returns a regex that matches with given file extensions
-	if type(ext) ~= 'table' then ext = {ext} end
-	return vim.regex([[\.\(]]..table.concat(ext, [[\|]])..[[\)$]])
-end
+local vim_cmd = vim.cmd
 
 if conf.trim_trailing_whitespace then
+	local function regex_ext(ext) -- returns a regex that matches with given file extensions
+		if type(ext) ~= 'table' then ext = {ext} end
+		return vim.regex([[\.\(]]..table.concat(ext, [[\|]])..[[\)$]])
+	end
 	autocmd('BufWritePre', {
 		desc = 'Trim trailing whitespace on save',
 		pattern = '*',
 		callback = function(ctx)
 			local ignore = {'md'}
 			if regex_ext(ignore):match_str(ctx.file) then return end
-			vim.cmd([[silent! %s/\s\+$//g]]) -- trim
+			vim_cmd([[silent! %s/\s\+$//g]]) -- trim
 		end
 	})
 end
@@ -33,21 +33,21 @@ if conf.detect_large_file then
 			local size = vim.fn.getfsize(ctx.file)
 			if size > conf.detect_large_file_size then
 				if vim.fn.exists(':TSBufDisable') ~= 0 then -- disable treesitter modules
-					vim.cmd('TSBufDisable autotag')
-					vim.cmd('TSBufDisable highlight')
-					vim.cmd('TSBufDisable incremental_selection')
-					vim.cmd('TSBufDisable indent')
-					vim.cmd('TSBufDisable playground')
-					vim.cmd('TSBufDisable query_linter')
-					vim.cmd('TSBufDisable rainbow')
-					vim.cmd('TSBufDisable refactor.highlight_current_scope')
-					vim.cmd('TSBufDisable refactor.highlight_definitions')
-					vim.cmd('TSBufDisable refactor.navigation')
-					vim.cmd('TSBufDisable refactor.smart_rename')
-					vim.cmd('TSBufDisable textobjects.swap')
-					--vim.cmd('TSBufDisable textobjects.move')
-					vim.cmd('TSBufDisable textobjects.lsp_interop')
-					vim.cmd('TSBufDisable textobjects.select')
+					vim_cmd('TSBufDisable autotag')
+					vim_cmd('TSBufDisable highlight')
+					vim_cmd('TSBufDisable incremental_selection')
+					vim_cmd('TSBufDisable indent')
+					vim_cmd('TSBufDisable playground')
+					vim_cmd('TSBufDisable query_linter')
+					vim_cmd('TSBufDisable rainbow')
+					vim_cmd('TSBufDisable refactor.highlight_current_scope')
+					vim_cmd('TSBufDisable refactor.highlight_definitions')
+					vim_cmd('TSBufDisable refactor.navigation')
+					vim_cmd('TSBufDisable refactor.smart_rename')
+					vim_cmd('TSBufDisable textobjects.swap')
+					--vim_cmd('TSBufDisable textobjects.move')
+					vim_cmd('TSBufDisable textobjects.lsp_interop')
+					vim_cmd('TSBufDisable textobjects.select')
 				end
 				if size > conf.detect_large_file_size_bigger then
 					vim.bo.syntax = 'OFF'
@@ -69,7 +69,7 @@ if conf.clear_jumplist then
 	autocmd('VimEnter', {
 		desc = 'Clear jumplist on start',
 		callback = function()
-			vim.cmd.clearjumps()
+			vim_cmd.clearjumps()
 		end
 	})
 end
@@ -78,7 +78,7 @@ if conf.auto_stopinsert then
 	autocmd({'FocusLost', 'WinLeave'}, {
 		desc = 'Automatically escape insert mode',
 		callback = function()
-			if vim.fn.mode() ~= 'c' then vim.cmd.stopinsert() end
+			if vim.fn.mode() ~= 'c' then vim_cmd.stopinsert() end
 		end
 	})
 end
