0765093b9f0cb29656a6970aee0f88776583780e
Satoshi Soma <noreply@amekusa.com>
2026-06-10 02:50:23 +0900

    snippets: (wip) migrating from vscode to snipmate

--- plugins.lua
+++ plugins.lua
@@ -343,8 +343,8 @@ local plugins = { -- in alphabetical order (ignore 'nvim-' prefix)
 		'L3MON4D3/LuaSnip', enabled = true,
 		build = 'make install_jsregexp',
 		config = function()
-			require('luasnip.loaders.from_vscode').lazy_load({
-				paths = {my.root..'snippets'}
+			require('luasnip.loaders.from_snipmate').lazy_load({
+				paths = {my.root..'snippets/snipmate'}
 			})
 		end
 	},
